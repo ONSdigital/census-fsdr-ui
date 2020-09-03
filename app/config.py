f0c45e123da44f34d9fa7e1f0ad9cf012b75b816
ThomasPoot <35296806+thomaspoot@users.noreply.github.com>
2020-09-03 15:08:33 +0100

    Added in test SAML files and a slightly adjusted config

--- app/config.py
+++ app/config.py
@@ -121,4 +121,4 @@ class TestingConfig:
     SESSION_AGE = ''
 
     SECRET_KEY = 'examplesecretkey'
-    SSO_CONFIG_FOLDER = env('SSO_CONFIG_FOLDER', default=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'saml'))
+    SSO_CONFIG_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file)), '..', 'test_saml')
