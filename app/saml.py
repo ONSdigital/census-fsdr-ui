804943cb7b76ebfb3d329291a89a657be043f60a
ThomasPoot <35296806+thomaspoot@users.noreply.github.com>
2020-09-22 14:58:41 +0100

    Corrected format of redirect param

--- app/saml.py
+++ app/saml.py
@@ -72,7 +72,7 @@ async def sso(request):
     # return HTTPFound(sso_built_url)
 
     # If we need to redirect to a specific URL:
-    return_to = '%sindex/' % request.url
+    return_to = '{}://{}/index'.format(request.scheme, request.host)
     raise HTTPFound(auth.login(return_to))
 
 
