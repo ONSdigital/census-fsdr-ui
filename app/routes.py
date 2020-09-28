7682824240a49e38955201f1cc6cbb23146a0f8b
ThomasPoot <35296806+thomaspoot@users.noreply.github.com>
2020-09-28 11:03:45 +0100

    Added extra routes in routes.py for common redirects. Currently only / into /signin.

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from aiohttp.web import HTTPFound, RouteTableDef
 from aiohttp_utils.routing import add_resource_context, add_route_context
 
 from .employeehandler import employee_routes
@@ -7,18 +8,31 @@ from .saml import saml_routes
 from .searchhandler import search_routes
 
 
+extra_routes = RouteTableDef()
+
+
+@extra_routes.get('/')
+async def root(request):
+    raise HTTPFound(request.app.router['sso'].url_for())
+
+
 def setup(app, url_path_prefix):
     """Set up routes as resources so we can use the `Index:get` notation for URL lookup."""
 
-    combined_routes = [*employee_routes, *index_route, *static_routes, *search_routes, *saml_routes]
+    combined_routes = [
+        *extra_routes, *employee_routes, *index_route, *static_routes,
+        *search_routes, *saml_routes
+    ]
 
     module = ('app.handler')
     for route in combined_routes:
-        prefix = url_path_prefix if route.kwargs.get('use_prefix', True) else ''
+        prefix = url_path_prefix if route.kwargs.get('use_prefix',
+                                                     True) else ''
         if route.method == "*":
-            with add_resource_context(app, module=module, url_prefix=prefix) as new_resource:
+            with add_resource_context(app, module=module,
+                                      url_prefix=prefix) as new_resource:
                 new_resource(route.path, route.handler())
         else:
-            with add_route_context(app, module=module, url_prefix=prefix) as new_route:
+            with add_route_context(app, module=module,
+                                   url_prefix=prefix) as new_route:
                 new_route(route.method, route.path, route.handler)
-
