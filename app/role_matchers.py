5af4b9842d950b97d754e195164ca570bbab893f
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2020-10-05 17:15:31 +0000

    d

--- app/role_matchers.py
+++ app/role_matchers.py
@@ -1,16 +1,22 @@
-
-
 import re
 from aiohttp.web import HTTPInternalServerError
 from structlog import get_logger
 
 from app import views
 
-
 logger = get_logger('fsdr-ui')
 
-
 rmt_regex = re.compile('R.-....(-..(-..)?)?')
+non_com_regex = re.compile('FT-NCP.-Z.-..')  # Non-Compliance in HQ
+self_help_regex = re.compile('LT-SHL.-..-..')  # Self-Help
+msg_service_regex = re.compile('LT-CFS.-..-..')  # messaging service
+health_safety_regex = re.compile('ZT-HSA.-..-..')  # health and safety
+rmt_combined_regex = re.compile('({}|{}|{}|{}|{})'.format(*[
+    r.pattern for r in [
+        rmt_regex, non_com_regex, self_help_regex, msg_service_regex,
+        health_safety_regex
+    ]
+]))
 hq_fo_ccs_regex = re.compile('F.-....-..-..')
 recruit_regex = re.compile('PT-FP[RP].-..-..')
 hr_regex = re.compile('PT-FPH.-..-..')
@@ -24,7 +30,7 @@ def invalid_role_id(role_id):
 
 
 def role_id_to_extract_type(role_id):
-    if rmt_regex.match(role_id):
+    if rmt_combined_regex.match(role_id):
         return 'RMT'
     # HQ, FO, CCS
     elif hq_fo_ccs_regex.match(role_id):
@@ -47,7 +53,7 @@ def role_id_to_extract_type(role_id):
 
 
 def role_id_to_view_router(role_id):
-    if rmt_regex.match(role_id):
+    if rmt_combined_regex.match(role_id):
         return views.rmt_view.get_employee_tabs
     # HQ, FO, CCS
     elif hq_fo_ccs_regex.match(role_id):
@@ -67,4 +73,3 @@ def role_id_to_view_router(role_id):
     # Failed to match
     else:
         invalid_role_id(role_id)
-
