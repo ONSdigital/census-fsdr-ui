90ebac289e7ad692b4b73983d8b6ad3fb332ddb7
ThomasPoot <35296806+thomaspoot@users.noreply.github.com>
2020-10-07 16:01:02 +0000

    Added a dummy payroll regex, which isn't used yet. The recruitment regex already includes this role id.

--- app/role_matchers.py
+++ app/role_matchers.py
@@ -12,6 +12,7 @@ logger = get_logger('fsdr-ui')
 
 rmt_regex = re.compile('R.-....(-..(-..)?)?')
 hq_fo_ccs_regex = re.compile('F.-....-..-..')
+payroll_regex = re.compile('PT-FPP.-..-..') # unused, see below
 recruit_regex = re.compile('PT-FP[RP].-..-..')
 hr_regex = re.compile('PT-FPH.-..-..')
 fsss_regex = re.compile('(ZT-HSA.-.|LT-CFS1-Z|DT-SUP.-.).-..')
