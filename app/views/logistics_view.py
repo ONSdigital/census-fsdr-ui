ae4d70f3cb0e05e8290154f1e7d5b6d8012eff54
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2020-10-30 15:11:44 +0000

    Added importe to allow UK_DATE_FORMATTING function  to work

--- app/views/logistics_view.py
+++ app/views/logistics_view.py
@@ -1,6 +1,8 @@
 from app.employee_view_functions import process_device_details, format_line_manager
-from app.tabutils import tab_generation
+from app.tabutils import tab_generation, format_to_uk_dates
 from app.fieldmapping import map_employee_name
+
+
 
 
 
