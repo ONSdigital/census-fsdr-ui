d28865fba49f654d48da5780815465f41658cebe
ThomasPoot <35296806+thomaspoot@users.noreply.github.com>
2020-11-12 11:07:43 +0000

    Minor improvements to continuous image

--- scripts/get_and_run_latest_dist.sh
+++ scripts/get_and_run_latest_dist.sh
@@ -1,6 +1,6 @@
 # Get code fromthe branch specified as environment variable
-git clone http://github.com/ONSDigital/census-fsdr-ui .
-git checkout -b $BRANCH
+git clone --branch $BRANCH http://github.com/ONSDigital/census-fsdr-ui /opt/repo
+cp -a /opt/repo/fsdr-ui/* .
 
 # Install  local python dependancies
 pipenv install --deploy --system
