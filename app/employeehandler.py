27faad03b7f527fc844ba3de39644ceb54f2b554
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2020-12-21 11:03:51 +0000

    Made .get for dict so if no key then it will return None

--- app/employeehandler.py
+++ app/employeehandler.py
@@ -96,7 +96,7 @@ class EmployeeInformation():
                 device_info = device_info + device_info_json.copy()
 
         last_job_role = employee_info['lastRoleId']
-        job_role_info = employee_info['jobRoles']
+        job_role_info = employee_info.get('jobRoles')
         relevant_job_role = ''
 
         for job_role in job_role_info:
