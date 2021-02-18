5171e5c3960f7761369588852f9686eb91da5c26
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-02-18 15:04:03 +0000

    Converted device table into a microservice

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,6 @@ from .handler import static_routes
 from .saml import saml_routes
 from .searchhandler import search_routes
 from .interfaceactiontablehandler import interface_action_handler_table_routes
-from .devicetablehandler import device_table_handler_routes
 from .downloadshandler import downloads_routes
 from .microserviceshandler import microservices_handler_routes
 
@@ -39,7 +38,6 @@ def setup(app, url_path_prefix):
       *search_routes,
       *saml_routes,
       *interface_action_handler_table_routes,
-      *device_table_handler_routes,
       *downloads_routes,
       *microservices_handler_routes,
   ]
