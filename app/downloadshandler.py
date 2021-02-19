098e4ce6100fa5dacbdfefaaf5b9671a8643ca13
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-02-19 13:36:03 +0000

    Fixed issues with download (broken from changing IAT to microservice)

--- app/downloadshandler.py
+++ app/downloadshandler.py
@@ -14,7 +14,7 @@ from app.role_matchers import download_permission
 from app.error_handlers import client_response_error, warn_invalid_login
 
 from app.searchfunctions import (get_all_assignment_status,
-                                 get_employee_records,
+                                 get_microservice_records,
                                  iat_employee_record_table,
                                  iat_employee_table_headers,
                                  get_distinct_job_role_short,
@@ -68,16 +68,18 @@ class DownloadsPage:
     try:
       search_range, records_per_page = page_bounds(1)
 
-      get_employee_info = get_employee_records(search_range, iat=True)
+      search_range = {'rangeHigh': 10, 'rangeLow': 0}
+      get_employee_info = get_microservice_records("iattable",
+                                                   user_filter=search_range)
       get_employee_info_json = get_employee_info.json()
 
       if len(get_employee_info_json) > 0:
-        employee_sum = get_employee_info_json[0].get('total_employees', 0)
+        employee_sum = get_employee_info_json[0].get('total_records', 0)
 
         # If there are more than 0 people in iat
         # then get_employee_info is set to everyone (max is total number of employees)
         search_range = {'rangeHigh': employee_sum, 'rangeLow': 0}
-        get_employee_info = get_employee_records(search_range, iat=True)
+        get_employee_info = get_microservice_records("iattable", search_range)
         get_employee_info_json = get_employee_info.json()
 
         html_employee_records = iat_employee_record_table(
