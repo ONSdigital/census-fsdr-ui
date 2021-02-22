c2460c2f010376b3633dbb60cb081975e01ffd91
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-02-22 17:00:23 +0000

    fixed bugs, layed the foundation to put in accurate userlevel  permissions for  each view

--- app/microservice_views.py
+++ app/microservice_views.py
@@ -1,21 +1,27 @@
+from app.role_matchers import (
+    microservices_permissions,
+    has_download_permission,
+    )
+
 
 class View:
   def __init__(self,
       database_name,
       user_role,
       display_name=None,
-      url  = None,
-      who_can_view    =[],)
+      who_can_view=None,
+      url  = None,):
 
-  self.database_name = database_name
-  self.display_name = create_display_name(display_name)
-  self.url  = create_url(url, database_name)
-  self.currently_visible = visible(user_role, who_can_view)
-  self.download_available = download_available(user_role, database_name)
+    self.database_name = database_name
+    self.display_name = self.create_display_name(display_name)
+    self.url_clear  = self.create_url(url, database_name, clear='clear')
+    self.url = self.create_url(url, database_name, clear='')
+    self.currently_visible = self.visible(user_role, database_name, who_can_view)
+    self.download_available = self.download_available(user_role, database_name)
 
-  def create_url(self, url, database_name):
-    if display_name == None:
-      url = (f'/microservices/{database_name}clear')
+  def create_url(self, url, database_name, clear):
+    if url == None:
+      url = (f'/microservices/{database_name}{clear}')
     return url
       
   def create_display_name(self, display_name):
@@ -24,24 +30,30 @@ class View:
     return (display_name)
 
   def download_available(self, user_role, database_name):
+    return has_download_permission(user_role, database_name)
+
+  def visible(self, user_role, database_name, who_can_view):
+    return microservices_permissions(user_role, database_name)
 
 
-  def visible(self, user_role, who_can_view):
-
-
-def get_views():
+def get_views(user_role, microservice_name):
   views = []
 
-  views.append( View("gsuitetable"),)
-  views.append( View("iattable"),)
-  views.append( View("devicetable"),)
-  views.append( View("xmatable"),)
-  views.append( View("lwstable"),)
-  views.append( View("servicenowtable"),)
-  views.append( View("updatestatetable"),)
-  views.append( View("requestlogtable"),)
-  views.append( View("chromebooktable"),)
+  views.append( View("gsuitetable",user_role),)
+  views.append( View("iattable",user_role),)
+  views.append( View("devicetable",user_role),)
+  views.append( View("xmatable",user_role),)
+  views.append( View("lwstable",user_role),)
+  views.append( View("servicenowtable",user_role),)
+  views.append( View("updatestatetable",user_role),)
+  views.append( View("requestlogtable",user_role),)
+  views.append( View("chromebooktable",user_role),)
 
-  return (views)
+  current_view_index = 0
+  for counter, view in enumerate(views):
+    if view.database_name == microservice_name:
+      current_view_index = counter
+
+  return (views, current_view_index)
 
 
--- app/microserviceshandler.py
+++ app/microserviceshandler.py
@@ -9,7 +9,8 @@ from aiohttp_session import get_session
 from structlog import get_logger
 from app.pageutils import page_bounds, get_page, result_message
 from app.error_handlers import client_response_error, forbidden
-from app.role_matchers import has_download_permission, microservices_permissions
+from app.role_matchers import microservices_permissions
+from app.microservice_views import get_views
 
 from app.microservice_tables import (
     get_table_headers,
@@ -123,20 +124,15 @@ class MicroservicesTable:
       result_message_str = result_message(search_range, microservice_sum,
                                           microservice_title)
 
-      download_perm = has_download_permission(
-          user_role,
-          microservice_name,
-      )
+      views, current_view_index = get_views(user_role, microservice_name)
 
       return {
+          'views': views,
+          'current_view': views[current_view_index],
           'called_from_index': False,
-          'download_button_enabled': download_perm,
           'Fields': field_classes,
-          'microservice_name': microservice_name,
-          'microservice_title': microservice_title,
           'result_message': result_message_str,
           'page_title': f'{microservice_title} view for: {user_role}',
-          'dst_download': has_download_permission(user_role),
           'page_number': page_number,
           'last_page_number': max_page,
           'table_headers': table_headers,
@@ -200,20 +196,15 @@ class MicroservicesTable:
       result_message_str = result_message(search_range, microservice_sum,
                                           microservice_title)
 
-      download_perm = has_download_permission(
-          user_role,
-          microservice_name,
-      )
+      views, current_view_index = get_views(user_role, microservice_name)
 
       return {
+          'views': views,
+          'current_view': views[current_view_index],
           'called_from_index': False,
           'Fields': field_classes,
-          'download_button_enabled': download_perm,
-          'microservice_name': microservice_name,
-          'microservice_title': microservice_title,
           'result_message': result_message_str,
           'page_title': f'{microservice_title} view for: {user_role}',
-          'dst_download': has_download_permission(user_role),
           'page_number': page_number,
           'last_page_number': max_page,
           'table_headers': table_headers,
