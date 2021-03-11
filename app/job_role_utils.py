03363ef4ebea6a4872280a0b9f8939daa043aab7
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-03-11 15:06:09 +0000

    Increased effeciency of multithreaded caching

--- app/job_role_utils.py
+++ app/job_role_utils.py
@@ -2,21 +2,25 @@ import asyncio
 from aiohttp_session import get_session
 from app.utils import FSDR_USER, FSDR_URL, FSDR_PASS
 from aiohttp import BasicAuth
+from structlog import get_logger
 import time
 
+logger = get_logger('fsdr-ui')
 
 class JRNamesService:
   """Service to fetch and cache Job Role names for use in dropdown menus"""
   def __init__(self):
-    self.cache = None
+    self.cache = []
     self.lifetime = 30 * 60
-    self.timestamp = None
+    self.timestamp = 0
     self.lock = asyncio.Lock()
 
   async def fetch(self, request):
     client = request.app['client']
-    if self.cache is None or (time.time() - self.timestamp) > self.lifetime:
+    refresh_due = (time.time() - self.timestamp) > self.lifetime
+    if refresh_due and not self.lock.locked():
       async with self.lock:
+        logger.info("Refreshing Job Role Dropdown Cache")
         url = FSDR_URL + f'/jobRoles/allJobRoleShorts/distinct'
         auth = BasicAuth(FSDR_USER, FSDR_PASS)
         async with client.get(url, auth=auth) as resp:
